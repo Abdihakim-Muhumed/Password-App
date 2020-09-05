e1fce8dc88d958295cd5a4d3853b8937329ab845
“Abdihakim <abdihakim.0017@gmail.com.comgit config --global user.name abdihakim17git config --global user.email abdihakim.0017@gmail.com.com>
2020-09-05 10:32:52 +0300

    created class user in classes.py

---
+++ classes.py
@@ -0,0 +1,8 @@
+class User:
+    """user class"""
+
+    def __init__(self, userName,password):
+        '''Method for creating a user object'''
+        self.userName = userName
+        self.password = password
+        
