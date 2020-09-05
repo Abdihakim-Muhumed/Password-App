cf33e311249cb9e14302bfb8350c8e991e98c193
“Abdihakim <abdihakim.0017@gmail.com.comgit config --global user.name abdihakim17git config --global user.email abdihakim.0017@gmail.com.com>
2020-09-05 10:47:55 +0300

    Created credential class in classes.py'

--- classes.py
+++ classes.py
@@ -5,4 +5,14 @@ class User:
         '''Method for creating a user object'''
         self.userName = userName
         self.password = password
-        
+
+
+
+class Credential:
+    '''user credentials class'''
+
+    def __init__(self, name,user_name,pass_word):
+        '''Method to instantiate credential objec'''
+        self.name = name
+        self.user_name = user_name
+        self.pass_word = pass_word
