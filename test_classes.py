888e0e4c0324b2a527bc8a3ef17c75c65afe6de9
“Abdihakim <abdihakim.0017@gmail.com.comgit config --global user.name abdihakim17git config --global user.email abdihakim.0017@gmail.com.com>
2020-09-05 11:19:50 +0300

    Create TestUser class in test_classes.py

---
+++ test_classes.py
@@ -0,0 +1,14 @@
+import unittest
+import pyperclip
+from classes import User
+from classes import Credential
+
+
+class TestUser(unittest.Testcase):
+    """TestUser class to test User methods/behaviours
+    arg:
+     unittest.Testcase:  Testcase class that provides test methods.
+    """
+
+    def __init__(self, arg):
+        
